351222b276cccf214ceb28e93e15e4be2a633556
Tanishka Vaswani <tanishkavaswani1@gmail.com>
2021-01-13 12:21:51 +0530

    Reversing digits in array

---
+++ array1.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int size;
+    cin>>size;
+    
+    int a[size];
+    
+    for(int i=0; i<size; i++)
+    {
+        cin>>a[i];
+    }
+    
+    for(int i=size-1; i>=0; i--)
+    {
+        cout<<a[i]<<" "; 
+    }
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
+
